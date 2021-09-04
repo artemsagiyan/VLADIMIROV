68af469bf6ba2809d4f6c702c9cb7dfca4ed631e
Сагиян Артем <artemsagian414@gmail.com>
2021-09-04 23:14:31 +0300

    si

--- problem_si.c
+++ problem_si.c
@@ -3,7 +3,26 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+void inssort(int *arr, int len);
+
+int moveright(int *arr, int key, int last);
+
 int main() {
 
     
+}
+
+void inssort(int *arr, int len) {
+  int i;
+  
+  for (i = 0; i < len; ++i) {
+    int key, pos;
+    key = arr[i];
+    pos = moveright(arr, key, i);
+    arr[pos] = key;
+  }
+}
+
+int moveright(int *arr, int key, int last) {
+
 }
